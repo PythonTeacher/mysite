68ad561467d2e12cf17a26e598b2d8c5889e8329
pythonteacher <myha@dimigo.hs.kr>
2015-11-14 22:16:17 +0900

    .

---
+++ blog/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from django.shortcuts import get_object_or_404, redirect
+from .forms import PostForm
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)   # 폼에서 받은 데이터를 PostForm에 넘김
+
+        if form.is_valid():
+            # author 정보 추가를 위해 바로 Post 모델에 데이터를 저장하지 말기
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
